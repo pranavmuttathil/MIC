f10a1534bf21c715eb0279824287732e167e187b
Pranav MM <118104558+pranavmuttathil@users.noreply.github.com>
2023-09-28 00:37:18 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column,Integer,String,create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+engine = create_engine('sqlite:///database.db')
+Session = sessionmaker(bind=engine)
+session = Session()
