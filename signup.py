ce36947dad0b480293f930b8ad481bc2c5750f11
Pranav MM <118104558+pranavmuttathil@users.noreply.github.com>
2024-04-15 01:07:12 +0530

    Delete signup.py

--- signup.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint,Flask,render_template, request 
-from .sample import person
-
-register = Blueprint('signup', __name__)
-@register.route('/signup')
-
-def home():
-    return render_template('signup.html')
